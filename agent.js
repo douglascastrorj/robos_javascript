0ac70161151a576713db1cac62c8df6199145146
Douglas Castro <douglascastrorj@gmail.com>
2019-01-26 01:01:29 -0200

    mudanças para alg. genetico

--- agent.js
+++ agent.js
@@ -1,26 +1,54 @@
 
-function Vehicle(x, y, dna) {
-    this.acceleration = createVector(0, 0);
-    
-    //setting velocity
-    var v = createVector(random(-2,2), random(-2,2));
-    v.normalize();
-    this.velocity = v;
+class Vehicle {
 
-    this.position = createVector(x, y);
-    this.r = 4;
-    this.maxspeed = 5;
+    constructor(x, y, dna) {
+        this.acceleration = createVector(0, 0);
 
-    var sensor1 = this.velocity.copy();
-    sensor1.normalize();
-    sensor1.mult(60)
-    this.sensores = [ sensor1.copy().rotate(-0.15),  sensor1, sensor1.copy().rotate(0.15) ]
+        //setting velocity
+        var v = createVector(random(-2, 2), random(-2, 2));
+        v.normalize();
+        this.velocity = v;
 
-    //pesos para calcular funcao de decidir acao
-    this.dna = dna;
+        this.position = createVector(x, y);
+        this.r = 4;
+        this.maxspeed = 5;
+
+        var sensor1 = this.velocity.copy();
+        sensor1.normalize();
+        sensor1.mult(60)
+        this.sensores = [sensor1.copy().rotate(-0.2), sensor1, sensor1.copy().rotate(0.2)]
+
+        //matriz Q
+        this.dna = dna;
+
+        this.lifeTime = 0.0;
+        this.steps = 0.0;
+        this.countAction1 = 0.0;
+        this.countAction2 = 0.0;
+    }
+
+    getExp() {
+
+        let exp = this.lifeTime * 0.3 + this.steps * 0.7;
+
+        //aumentar exp se tomar todos os tipos de acoes
+        let mean = (this.countAction1 + this.countAction2) / 2;
+        if( abs(this.countAction1 - mean) >  0.7 ||  abs(this.countAction2 > mean) > 0.7  ){
+            exp = exp / 2;
+        }
+
+        //reduzir experiencia ganha se ele viver muito mais do que andar
+        if (this.lifeTime > this.steps * 3)
+            exp = exp / 2;
+
+        return exp;
+    }
+
 
     // Method to update location
-    this.update = function () {
+    update() {
+
+        this.lifeTime += 0.001;
 
         // Update velocity
         this.velocity.add(this.acceleration);
@@ -31,35 +59,43 @@ function Vehicle(x, y, dna) {
         this.acceleration.mult(0);
     }
 
-    this.rotate = function(angle) {
+    rotate(angle) {
         this.velocity.rotate(angle);
-        for(var i = 0; i < this.sensores.length; i++){
+        for (var i = 0; i < this.sensores.length; i++) {
             this.sensores[i].rotate(angle);
         }
     }
 
 
-    this.applyForce = function (force) {
+    applyForce(force) {
         // We could add mass here if we want A = F / M
         this.acceleration.add(force);
     }
 
-    this.escolherAcao = function(leitura) {
-        let sum = 0;
+    escolherAcao(state) {
+        var actions = this.dna[state];
 
-        if(leitura.length != this.dna.length){
-            throw 'Erro ao escolher acao baseado na leitura! leitura.length deve ser igual ao dna.length'
+        let maiorIndex = 0;
+        for (var i = 0; i < actions.length; i++) {
+            if (actions[i] > actions[maiorIndex]) {
+                maiorIndex = i;
+            }
         }
 
-        for(var i = 0; i < this.sensores.length; i++){
-            sum += this.dna[i] * leitura[i];
+        //se andou para frente somar steps
+        if (maiorIndex == 0) {
+            this.steps += 0.001;
+        } else if( maiorIndex == 1){
+            this.countAction1 += 0.001;
+        } else {
+            this.countAction2 += 0.001;
         }
 
-        if( sum >= 0) return 1;
-        else return 0;
+        //retorna acao mais provavel
+        return maiorIndex;
     }
 
-    this.display = function () {
+    display() {
         // Draw a triangle rotated in the direction of velocity
         var angle = this.velocity.heading() + PI / 2;
 
@@ -83,22 +119,22 @@ function Vehicle(x, y, dna) {
         pop();
 
         push();
-            var gr = color(0, 255, 0);
-            var rd = color(255, 0, 0);
-            var col = gr;//lerpColor(rd, gr, this.health);
-            fill(col);
-            stroke(col);
-            strokeWeight(1);
-            //desenhar sensores
-            for(var i = 0; i < this.sensores.length; i++ ){
-                var sensor = this.sensores[i];
-                // line(0, 0, 0, -sensor.mag());
-                line(this.position.x, this.position.y, this.position.x + sensor.x, this.position.y + sensor.y);
-                // console.log(this.position.x, this.position.y, this.position.x + sensor.x, this.y + sensor.y);
-            }
+        var gr = color(0, 255, 0);
+        var rd = color(255, 0, 0);
+        var col = gr;//lerpColor(rd, gr, this.health);
+        fill(col);
+        stroke(col);
+        strokeWeight(1);
+        //desenhar sensores
+        for (var i = 0; i < this.sensores.length; i++) {
+            var sensor = this.sensores[i];
+            // line(0, 0, 0, -sensor.mag());
+            line(this.position.x, this.position.y, this.position.x + sensor.x, this.position.y + sensor.y);
+            // console.log(this.position.x, this.position.y, this.position.x + sensor.x, this.y + sensor.y);
+        }
         pop();
-        
-       
+
+
     }
 
 }
